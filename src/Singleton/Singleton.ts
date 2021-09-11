95a583fca0429d360b4fcf4d84fc603675949263
victorrovani <73619824+victorrovani@users.noreply.github.com>
2021-09-10 21:54:11 -0300

    Update Singleton.ts

--- src/Singleton/Singleton.ts
+++ src/Singleton/Singleton.ts
@@ -1,1 +1,20 @@
 
+export class Singleton {
+   
+    private static instance: Singleton;
+
+    public name?: string;
+  
+    private constructor() { 
+        this.name = 'Victor'
+    }
+
+    public static getInstance(): Singleton {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+
+        return Singleton.instance;
+    }
+
+}
